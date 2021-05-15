1e8f674cc40b18f5ee85716b8c7c508cb1ed3775
Christos Ploutarchou <cploutarchou@gmail.com>
2021-05-16 01:27:37 +0300

    Update get_post() function

--- app/views.py
+++ app/views.py
@@ -16,28 +16,38 @@ def get_posts():
         data = {'title': post.title, 'description': post.description}
         output.append(data)
 
-    return {'posts': output}
+    return {'status_code': 200, "content": {'posts': output}, "items": len(output)}
 
 
-@app.route('/posts/<id>')
-def get_post(post_id: str):
-    post = Posts.query.get_or_404(id)
-    return jsonify({'id': post.id, 'description': post.description})
+@app.route('/post/<post_id>', methods=['GET'])
+@app.route('/post/', methods=['GET'])
+def get_post(post_id=None):
+    if request.json is None:
+        return jsonify({
+            'status_code': 400, 'Error': 'Bad Request',
+            'Error Description': "Post id is required."})
+    post = Posts.query.filter_by(id=post_id).first()
+    if post is None:
+        return jsonify({'status_code': 200, 'Error': "NO VALID POST ID."})
+    return jsonify({
+        'status_code': 200,
+        'content': {'id': post.id, 'description': post.description},
+        "message": f""
+    })
 
 
-# insert_drinks()
 @app.route('/posts', methods=['POST'])
 def create_post():
     post = Posts(title=request.json['title'], description=request.json['description'],
                  published=request.json['published'])
     db.session.add(post)
     db.session.commit()
-    return {'id': post.id, 'status': 200}
+    return {'id': post.id, 'status_code': 200, "message": f"Post id : {post.id} successfully created."}
 
 
-@app.route('/posts/<id>', methods=['PUT'])
-def update_post():
-    if id not in request.json:
+@app.route('/posts/<post_id>', methods=['PUT'])
+def update_post(post_id=None):
+    if post_id is None:
         return "Unable to update post. Post id is required."
     else:
         post = Posts.query.filter_by(id=request.json['id']).first()
@@ -46,4 +56,4 @@ def update_post():
                 continue
             post.item = request.json[item]
             db.session.commit()
-    return {'id': post.id, 'status': 200}
+    return {'id': post.id, 'status_code': 200, "message": f"Post id : {post.id} successfully updated."}
