a8d8adec8f2f62bb8017f344a1c2a135b9a7b78e
Christos Ploutarchou <cploutarchou@gmail.com>
2021-05-16 19:27:24 +0300

    Corrections

--- app/models.py
+++ app/models.py
@@ -5,13 +5,13 @@ class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, index=True)
     email = db.Column(db.String(120), unique=True, index=True)
-    full_name = db.Column(db.String(220))
+    name = db.Column(db.String(220))
 
     def __repr__(self):
         return f"id={self.id}, username={self.username}, " \
                f"email={self.email},first_name={self.first_name}," \
                f" last_name={self.last_name}", \
-               f"full_name={self.full_name}"
+               f"full_name={self.name}"
 
 
 class Posts(db.Model):
